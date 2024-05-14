349a7d6ef17966a1edf67f5b3a48aa0b2f76d7f9
Edwin <edwinadradaruiz99@gmail.com>
2024-05-13 21:23:24 -0500

    Implementacion del query's en CuestionarioRepository

--- Taller-6/src/main/java/co/edu/unicauca/asae/Taller6/infrastructure/output/persistencia/repositories/ICuestionarioRepository.java
+++ Taller-6/src/main/java/co/edu/unicauca/asae/Taller6/infrastructure/output/persistencia/repositories/ICuestionarioRepository.java
@@ -1,8 +1,17 @@
 package co.edu.unicauca.asae.Taller6.infrastructure.output.persistencia.repositories;
 
 import co.edu.unicauca.asae.Taller6.infrastructure.output.persistencia.entities.CuestionarioEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ICuestionarioRepository extends CrudRepository<CuestionarioEntity, Integer> {
+
+   List<CuestionarioEntity> findByTituloContainingIgnoreCaseOrderByIdDesc(String titulo);
+
+   @Query("SELECT COUNT(c) > 0 FROM Cuestionario c WHERE LOWER(c.titulo) = LOWER(:titulo)")
+   boolean existsByTitulo(@Param("titulo") String titulo);
 
 }
